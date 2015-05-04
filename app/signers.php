6734d1a8b1a9c29d7af4def27a4b00f0fa8a446a
Andrew Stanish <andy@youshallthrive.com>
2015-05-04 01:47:10 -0400

    updated form and added values for live site to URLs (have hosts file set locally and a vhost pointing at the dist dir; need to get Gruntfile to install bootstrap, need to install map

--- app/signers.php
+++ app/signers.php
@@ -1,4 +1,5 @@
 <?php
+// UNCOMMENT ALL THIS:
 /*define('IS_AJAX', isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest');*/
 //if(!IS_AJAX) {
   //header("Location: http://" . $_SERVER['HTTP_HOST']);
@@ -9,9 +10,10 @@
 header("Access-Control-Allow-Origin: *");
 // YES THIS ^^^^
 
-require '../vendor/autoload.php';
-Dotenv::load('..');
+require '../vendor/autoload.php'; // grab the composer classes
+Dotenv::load('..'); // grab the db connection info out of the environment
 
+// set up the database connection; using Doctrine for future-proof-ness
 $config = new \Doctrine\DBAL\Configuration();
 
 $connectionParams = array(
@@ -21,17 +23,45 @@ $connectionParams = array(
   'host'     => $_ENV['BERNIE_DB_HOST'],
   'driver'   => 'pdo_mysql',
 );
+
 $conn = \Doctrine\DBAL\DriverManager::getConnection($connectionParams, $config);
 
-if( isset($_POST['email']) ) {
-  $sql = "INSERT INTO `bernie_db`.`bernie_signers` (`email`, `name`, `zip`, `get_involved`) VALUES ('?', '?', '?', '?')";
-  $stmt = $conn->prepare($sql);
-  $stmt->bindValue(1, $_POST['email']);
-  $stmt->bindValue(2, $_POST['name']);
-  $stmt->bindValue(3, $_POST['zipcode']);
-  $stmt->bindValue(4, $_POST['getInvolved']);
-  $stmt->execute();
+// SQL query to count number of rows
+$countSQL = "SELECT COUNT(*) FROM bernie_signers";
+
+// handle POST: add person to database
+if( isset($_POST['zipcode']) ) {
+
+  $conn->beginTransaction(); //wrapping in a transaction for future-proof-ness
+
+  try {
+
+    $conn->insert('bernie_signers', array(
+      'email'        => (isset($_POST['email']) ? $_POST['email'] : '' ),
+      'name'         => (isset($_POST['name']) ? $_POST['name'] : ''),
+      'zip'          => $_POST['zipcode'],
+      'get_involved' => (isset($_POST['getInvolved']) ? 1 : 0 )),
+      array(\PDO::PARAM_STR, \PDO::PARAM_STR, \PDO::PARAM_INT, \PDO::PARAM_BOOL)
+    );
+
+  } catch(Exception $e) {
+
+    $conn->rollback(); // if it fails, rollback the changes..
+    error_log($e); // log the error and give the ajax call an error message
+    echo "We're sorry, there's been an error. Please try again soon!";
+    die(); // and then die
+
+  }
+
+  // success: give the ajax call the updated total
+  $results = $conn->fetchColumn($countSQL, array(1), 0);
+  echo "You're person number " . $results . " to say that Bernie Can Win!";
+  die();
+
+// or just display the results
 } else {
-  $results = $conn->fetchColumn("SELECT COUNT(*) FROM bernie_signers", array(1), 0);
+
+  $results = $conn->fetchColumn($countSQL, array(1), 0);
   echo $results;
+
 }
